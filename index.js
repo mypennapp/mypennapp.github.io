1e9c0be9e635a5c299b4764769e0008426089627
mypennapp <fremont.techtutors@gmail.com>
2015-08-19 05:26:06 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function size() {
         amt = $(window).height() -$("#bottom").outerHeight() - $("header").height()
         console.log(amt);
         $("#bottom").css({
-            'margin-bottom': -470
+            'margin-bottom': -490
         });
     } else {
         $("#bottom").css({

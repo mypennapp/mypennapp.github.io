fc7705edfab07c68a122d2fe6d70bfa9c4651f6f
Mohammed Abdulwahhab <furkhan324@mohammeacintosh.gateway.pace.com>
2015-08-19 05:16:06 -0700

    hello10

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function size() {
         amt = $(window).height() -$("#bottom").outerHeight() - $("header").height()
         console.log(amt);
         $("#bottom").css({
-            'margin-bottom': -454
+            'margin-bottom': -470
         });
     } else {
         $("#bottom").css({
